b384194c66676f6b01e30a5dece3eeb1898b2f7f
Drew Gorsline <dregors@gmail.com>
2020-01-23 21:30:17 -0800

    second commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="css_file.css" rel="stylesheet" type="text/css" media="all">
+<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono&display=swap" rel="stylesheet">
+</head>
+<body>
+    <h1>OMNI RSS</h1>
+	<h3>The owner of the Red Chrysler needs to readjust his blinders. Where's that scalpel? Susie Allen did not come home from school today on account of a skinned knee. China has a lot of delicious food. If you like my content and would like subscribe for more updates, kindly press the RSS icon.</h3>
+<a href="test.xml" class="feed">subscribe to our feed</a>
+</body>
+</html>
