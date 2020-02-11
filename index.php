690e75608d6540abe6e8f7c55de4ca08f2264a79
Ubuntu <ubuntu@ip-172-31-20-192.us-east-2.compute.internal>
2020-02-11 21:48:16 +0000

    more entries

--- index.php
+++ index.php
@@ -5,18 +5,20 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<style>
+        <style>
             @font-face {font-family: 'IBM Plex Mono', monospace;}
             body { font-family:'IBM-Plex-Mono', monospace; font-size: 170%; }
             a:visited { color: #a3bcd1; }
         </style>
     </head>
     <body>
-        <div class="container">
-            <div class="row">
+<div class="container">
+    <h1>Omni News</h1>
+            <div class="row">	
                 <div class="col-md-8" >
                 <?php
-					function getContent() {
+
+                    function getContent() {
                         //Thanks to https://davidwalsh.name/php-cache-function for cache idea
                         $file = "";
                         $current_time = time();
@@ -32,28 +34,75 @@
                             return $content;
                         }
                     }
-
-                    function getFreshContent() {
+		    function getFreshContent() {
                         $html = "";
-                        $newsSource = array(
+			$newsSource = array( 
                             array(
-                                "title" => "BBC",
-                                "url" => "http://feeds.bbci.co.uk/news/world/rss.xml"
+                                "title" => "The New York Times",
+                                "url" => "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+			    ),
+						  
+			    array(
+                                "title" => "Techmeme",
+                                "url" => "https://www.techmeme.com/feed.xml"
                             ),
-                            array(
-                                "title" => "CNN",
-                                "url" => "http://rss.cnn.com/rss/cnn_latest.rss"
+			    
+			    array(
+                                "title" => "ESPN Top Headlines",
+                                "url" => "https://www.espn.com/espn/rss/news"
                             ),
-                            array(
-                                "title" => "Fox News",
-                                "url" => "http://feeds.foxnews.com/foxnews/latest"
-                            ),
-                            array(
+			    
+			    array(
                                 "title" => "Hacker News",
                                 "url" => "https://news.ycombinator.com/rss"
-                            )
-                        );
-                        function getFeed($url){
+                            ),
+			    
+			    array(
+				"title" => "Fox News",
+				"url" => "http://feeds.foxnews.com/foxnews/latest"
+			    ),
+			    
+			    array(
+				"title" => "CNN",
+				"url" => "http://rss.cnn.com/rss/cnn_latest.rss"
+			    ),
+			    
+			    array(
+				"title" => "aljazeera",
+				"url" => "http://aljazeera.com/xml/rss/all.xml"
+			    ),
+			    
+			    array(
+				"title" => "Defence Blog",
+				"url" => "http://defence-blog.com/feed"
+			    ),
+			    
+			    array(
+				"title" => "E-International Relations",
+				"url" => "http://e-ir.info/category/blogs/feed"
+			    ),
+				
+			    array(
+				"title" => "The Cipher Brief",
+				"url" => "http://thecipherbrief.com/feed"
+			    ),
+				
+			    array(
+				"title" => "The Guardian",
+				"url" => "http://theguardian.com/world/rss"
+			    ),	
+
+		        );
+
+
+                        foreach($newsSource as $source) {
+                            $html .= '<h2>'.$source["title"].'</h2>';
+                            $html .= getFeed($source["url"]);
+                        }
+                        return $html;
+                    }
+			
+			function getFeed($url){
                             $html = "";
                             $rss = simplexml_load_file($url);
                             $html .= '<ul>';
@@ -63,15 +112,9 @@
                             $html .= '</ul>';
                             return $html;
                         }
-
-                        foreach($newsSource as $source) {
-                            $html .= '<h2>'.$source["title"].'</h2>';
-                            $html .= getFeed($source["url"]);
-                        }
-                        return $html;
-                    }
-                ?>
-                	<div class="bg-info" >  
+		?>
+		  
+                	<div class="bg-info">  
                 		<?php print getContent(); ?>
                 	</div>
                 </div>  
